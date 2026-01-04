e22cfd67e716ce56add88e5608dd774ef657e6d9
LAB3 <bindurjain160@gmail.com>
2026-01-04 11:40:42 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Add from './pages/Add'
+import View from './pages/View'
+import Edit from './pages/Edit'
+import Error from './pages/Error'
+//import './App.css'
+import { ToastContainer, toast } from 'react-toastify';
+
+function App() {
+  return<>
+  <ToastContainer />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<View />} />
+        <Route path='/add' element={<Add />} />
+        <Route path='/edit/:expenseId' element={<Edit />} />
+        {/*not found page*/}
+        <Route path='*' element={<Error />} />
+
+      </Routes>
+    </BrowserRouter>
+  </>
+
+}
+
+export default App
